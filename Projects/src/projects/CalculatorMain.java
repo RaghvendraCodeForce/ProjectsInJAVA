9e8935c6d203ce894ca5abd13188e8f8a5563c6a
raghvendra <raghvendra@10.156.227.30>
2024-06-21 16:39:22 +0530

    Calculator using two concepts + Balance Parentheses

---
+++ Projects/src/projects/CalculatorMain.java
@@ -0,0 +1,11 @@
+package projects;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		
+		Calculator cal1 = new Calculator();
+		
+		System.err.println(cal1.calculate("1+5"));
+	}
+}
